c6a7e79a755508579da17d0136ec20e9e792d542
humaneater <humaneater@163.com>
2023-05-29 01:13:22 +0800

    没太看懂lut怎么算的，感觉自己算一次更好，明天重算

--- Assets/Accumulation/Postprocess/AtmosphericScattering/AtmosphericScattingGenerate.cs
+++ Assets/Accumulation/Postprocess/AtmosphericScattering/AtmosphericScattingGenerate.cs
@@ -1,14 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class AtmosphericScattingGenerate : MonoBehaviour
 {
-    public ComputeShader ScatteringShader;
-
+    
+    //需要添加的数据
+    public float planetRadius = 6360f; 
+    public float AtmosphereTop = 6420f;
+    public int SampleCount = 500;
+    public ComputeShader ScatteringCS;
+    
     private static readonly int PlanetRadius = Shader.PropertyToID("_PlanetRadius");
 
     private RenderTexture TransparencyLUT;
+    private static readonly int TransparencyLUTID = Shader.PropertyToID("_TransparencyLUT");
+    private static readonly int GasDensity = Shader.PropertyToID("_GasDensity");
+    private static readonly int _AtmosphereTop = Shader.PropertyToID("_AtmosphereTop");
+    private static readonly int _SampleCount = Shader.PropertyToID("_SampleCount");
 
     private RenderTexture ScatteringLUT;
 
@@ -18,24 +29,37 @@ public class AtmosphericScattingGenerate : MonoBehaviour
 
     void Start()
     {
-        
+        InitData();
     }
 
     private void InitData()
     {
-        TransparencyLUT = new RenderTexture(256, 360,0, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Linear);
-        TransparencyLUTkernel = ScatteringShader.FindKernel("TransparenceLUTPass");
+        RenderTextureDescriptor descriptor = new RenderTextureDescriptor(256, 256, RenderTextureFormat.RGB111110Float);
+        descriptor.enableRandomWrite = true;
+        TransparencyLUT = new RenderTexture(descriptor);
         
     }
 
     private void GenerateLUT()
     {
-        
+        TransparencyLUTkernel = ScatteringCS.FindKernel("TransparencyLUTPass");
+        ScatteringCS.SetFloat(PlanetRadius,planetRadius);
+        ScatteringCS.SetFloat(_AtmosphereTop,AtmosphereTop);
+        ScatteringCS.SetInt(_SampleCount,SampleCount);
+        ScatteringCS.SetTexture(TransparencyLUTkernel,TransparencyLUTID,TransparencyLUT);
+        ScatteringCS.Dispatch(TransparencyLUTkernel,256/8,256/8,1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        GenerateLUT();
+    }
+
+    private void OnDisable()
+    {
+//        ScatteringLUT.Release();
+        TransparencyLUT.Release();
         
     }
 }
