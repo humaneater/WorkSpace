4e3010add29c543f8d315484c4b4cc4ded9c6f8f
humaneater <humaneater@163.com>
2023-06-09 01:07:55 +0800

    C#准备阶段做完，做个排序读出来就好了

--- Assets/Accumulation/Scene/Scripts/ComputeMultiLights.cs
+++ Assets/Accumulation/Scene/Scripts/ComputeMultiLights.cs
@@ -2,19 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ComputeMultiLights : MonoBehaviour
 {
-    public GameObject lightGroup;
-
-    private List<Light> mLightList;
-    private List<LightData> mCachedLight;
-
-    // Start is called before the first frame update
-    private int lightNumber;
-    private int nowCountNumber;
-    private bool mTrigger;
-
+    //拟定一个灯光结构体，因为要变化所以就用class制定了，不知道会不会很大
     private class LightData
     {
         private Color lightColor;
@@ -38,17 +30,49 @@ public class ComputeMultiLights : MonoBehaviour
             spotAngle = light.spotAngle;
         }
     }
+    //灯的相关数据
+    public GameObject lightGroup;
+    private List<Light> mLightList;
+    private List<LightData> mCachedLight;
+    private int lightNumber;
+    private int nowCountNumber;
+    //最起码做一个灯光是否需要更新的开关
+    private bool mLightUpdateTrigger;
+
+    //置脏就更新，否则不更新
+    private bool isDirty;
+    
+    //接下来重头戏，需要用cs做判断，做排序，做一张图，x是世界空间x，y是世界空间y，z是世界空间z，先用一个xy做分层吧，混入y可能需要制作一张3dtexture，会让查找变得得不偿失
+    public ComputeShader LightGetheringCS;
+    private static readonly string DrawkernelString = "DrawIndexTexture";
+    private ComputeBuffer LightPositionBuffer;
+    private List<Vector4> mLightPositionList;
+    private static readonly int ShaderLightPositionList = Shader.PropertyToID("_LightsPosition");
+    private ComputeBuffer LightDirectionBuffer;
+    private List<Vector4> mLightDirectionList;
+    private static readonly int ShaderLishgtDirectionList = Shader.PropertyToID("_LightsDirection");
+    private int DrawIndexTextureKernel;
+    //id图的rt
+    private RenderTexture LightIndexRT;
+    private static readonly int LightIndexRTID = Shader.PropertyToID("_LightIndexTex");
+    [SerializeField]private Vector3 TextureSize = new Vector3(256,256,32);
+    private static readonly int LightTextureSize = Shader.PropertyToID("_LightIndexTex_Size");
+    
+    
 
     void InitData()
     {
-        mTrigger = true;
+        mLightPositionList = new List<Vector4>();
+        mLightDirectionList = new List<Vector4>();
+        isDirty = false;
+        mLightUpdateTrigger = true;
         mLightList = new List<Light>();
         mCachedLight = new List<LightData>();
         mLightList.Clear();
         mLightList.AddRange(lightGroup.transform.GetComponentsInChildren<Light>());
+        ResetRT();
         InitCachedLightData();
-        lightNumber = mLightList.Count;
-        nowCountNumber = 0;
+        DrawIndexTexture();
         StartCoroutine(CheckIfLightChange());
     }
 
@@ -58,30 +82,72 @@ public class ComputeMultiLights : MonoBehaviour
         {
             mCachedLight.Add(new LightData(mLightList[i]));
         }
+        lightNumber = mLightList.Count;
+        nowCountNumber = 0;
     }
+
+    private void ResetRT()
+    {
+        RenderTextureDescriptor descriptor =
+            new RenderTextureDescriptor((int)TextureSize.x, (int)TextureSize.y, RenderTextureFormat.ARGB32);
+        descriptor.enableRandomWrite = true;
+        //思考：是不是3d也可以呢？如果本来用的就是point，那么3d又有什么消耗呢？
+        //descriptor.dimension = TextureDimension.Tex3D;
+        LightIndexRT = new RenderTexture(descriptor);
+    }
+
+    private void DrawIndexTexture()
+    {
+        DrawIndexTextureKernel = LightGetheringCS.FindKernel(DrawkernelString);
+        LightPositionBuffer = new ComputeBuffer(lightNumber, sizeof(float) * 4, ComputeBufferType.Default);
+        mLightPositionList.Clear();
+        LightDirectionBuffer = new ComputeBuffer(lightNumber, sizeof(float) * 4, ComputeBufferType.Default);
+        mLightDirectionList.Clear();
+        for (int i = 0; i < lightNumber; i++)
+        {
+            mLightPositionList.Add(mLightList[i].transform.position);
+            mLightDirectionList.Add(mLightList[i].transform.forward);
+        }
+        LightPositionBuffer.SetData(mLightPositionList);
+        LightDirectionBuffer.SetData(mLightDirectionList);
+        //向cs里输入需要的数据
+        LightGetheringCS.SetBuffer(DrawIndexTextureKernel,ShaderLightPositionList,LightPositionBuffer);
+        LightGetheringCS.SetBuffer(DrawIndexTextureKernel,ShaderLishgtDirectionList,LightDirectionBuffer);
+        LightGetheringCS.SetTexture(DrawIndexTextureKernel,LightIndexRTID,LightIndexRT);
+        LightGetheringCS.SetVector(LightTextureSize,TextureSize);
+        LightGetheringCS.Dispatch(DrawIndexTextureKernel,(int)(TextureSize.x/8),(int)(TextureSize.y/8),1);
+    }
+
+    private void Update()
+    {
+        DrawIndexTexture();
+    }
+
 
     private IEnumerator CheckIfLightChange()
     {
-        while (mTrigger)
+        while (mLightUpdateTrigger)
         {
             for (int i = 0; i < 2; i++)
             {
                 if (nowCountNumber == lightNumber)
                 {
                     nowCountNumber = 0;
+                    DoReset();
                     break;
                 }
 
                 if (mLightList[nowCountNumber].transform.hasChanged)
                 {
                     mLightList[nowCountNumber].transform.hasChanged = false;
-                    DoReset();
+                    isDirty = true;
                 }
 
                 if (mLightList[nowCountNumber].intensity != mCachedLight[nowCountNumber].GetIntensity())
                 {
                     mCachedLight[nowCountNumber].SetIntensity(mLightList[nowCountNumber].intensity);
-                    DoReset();
+                    isDirty = true;
+                    
                 }
 
                 nowCountNumber++;
@@ -97,7 +163,12 @@ public class ComputeMultiLights : MonoBehaviour
     private void DoReset()
     {
         //TODO:cs做图，重置rt，感觉不麻烦
-        Debug.Log("有变化了");
+        if (isDirty)
+        {
+            Debug.Log("有变化了");
+            isDirty = false;
+        }
+        
     }
 
     private void Start()
@@ -106,7 +177,8 @@ public class ComputeMultiLights : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
+        LightIndexRT.Release();
     }
 }
